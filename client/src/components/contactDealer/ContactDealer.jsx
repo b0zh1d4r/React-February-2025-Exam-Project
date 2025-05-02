e28919f5b932485ca53903c16c18ec6a20107e16
Bozhidar Ivanov <bozhidarivanov24@gmail.com>
2025-05-02 10:36:56 +0300

    Update ContactDealer.jsx

--- client/src/components/contactDealer/ContactDealer.jsx
+++ client/src/components/contactDealer/ContactDealer.jsx
@@ -33,13 +33,9 @@ export default function ContactDealer() {
             {error && <ErrorNotification message={error} clearError={() => setError("")} />}
             
             <div className="about-container">
-                <h2>Dealer's Information:</h2>
+                <h2>Dealer's Information:</h2>  
                 <p>Username: <span className="highlight">{dealer?.username || "N/A"}</span></p>
-                <p>Email: 
-                    <a href={`mailto:${dealer?.email}`}>
-                        <span className="highlight">{dealer?.email || "N/A"}</span>
-                    </a>
-                </p>
+                <p>Email: <a href={`mailto:${dealer?.email}`}>    <span className="highlight">{dealer?.email || "N/A"}</span></a></p>
                 <p>Phone Number: <span className="highlight">{dealer?.phoneNumber || "N/A"}</span></p>
                 <p>Location: <span className="highlight">{dealer?.location || "N/A"}</span></p>
             </div>
